05d7597ecd200878fb6220cc3ba8ff4c9392ed22
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 11:42:26 -0700

    uat port

--- uat.sh
+++ uat.sh
@@ -4,4 +4,4 @@ cp -r dist ~/Desktop/docker/ursaminor/
 cp nginx.conf ~/Desktop/docker/ursaminor/default.conf
 cd ~/Desktop/docker/ursaminor
 docker stop ursaminor
-docker run --rm -it -d --name ursaminor -p 61001:80 --network net -v ~/Desktop/docker/ursaminor/dist/:/usr/share/nginx/html/ -v ~/Desktop/docker/ursaminor/default.conf:/etc/nginx/conf.d/default.conf nginx
+docker run --rm -it -d --name ursaminor -p 61000:80 --network net -v ~/Desktop/docker/ursaminor/dist/:/usr/share/nginx/html/ -v ~/Desktop/docker/ursaminor/default.conf:/etc/nginx/conf.d/default.conf nginx
