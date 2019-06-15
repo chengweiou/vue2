68a699bc8b8dafaee1625216b9688284a9d571ba
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 16:52:42 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
 yarn build:uat
-rm -rf ~/Desktop/docker/ursaminor/dist
-cp -r dist ~/Desktop/docker/ursaminor/
-cp nginx.conf ~/Desktop/docker/ursaminor/default.conf
-cd ~/Desktop/docker/ursaminor
+rm -rf ~/Desktop/docker/universe/ursaminor/dist
+cp -r dist ~/Desktop/docker/universe/ursaminor/
+cp nginx.conf ~/Desktop/docker/universe/ursaminor/default.conf
+cd ~/Desktop/docker/universe/ursaminor
 docker stop ursaminor
-docker run --rm -it -d --name ursaminor -p 61000:80 --network net -v ~/Desktop/docker/ursaminor/dist/:/usr/share/nginx/html/ -v ~/Desktop/docker/ursaminor/default.conf:/etc/nginx/conf.d/default.conf nginx
+docker run --rm -it -d --name ursaminor -p 61000:80 --network net -v ~/Desktop/docker/universe/ursaminor/dist/:/usr/share/nginx/html/ -v ~/Desktop/docker/universe/ursaminor/default.conf:/etc/nginx/conf.d/default.conf nginx
