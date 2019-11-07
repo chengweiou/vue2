938f2c79e9818f64215b9f408fb2157f913a84d1
chengweiou <chengwei.ou.dora@gmail.com>
2019-11-07 15:50:20 -0800

    eslint

--- src/store/.eslintrc.js
+++ src/store/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     // inorder to keep template
     // { commit, dispatch, state, rootState }, payload, config = {}
     'no-unused-vars': 'off',
+    // show message for fetchUtil in predev
     'no-console': ['error', {allow: ['warn', 'error']}],
   },
 }
